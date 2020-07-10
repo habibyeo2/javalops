c2f0afa25bdbea0018352085b6ee12481868f3c3
Habib Yeo <habibyeo2@gmail.com>
2020-07-10 10:42:34 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var array = []
+for (var i = 1; i <= 10; i++){
+    array.push(i);
+}
+
+var array = []
+var i = 1;
+while (i <= 10){
+    array.push(i);
+    i++;
+}
+
+var array = []
+for (var i = 11; i <= 21; i += 2){
+    array.push(i);
+}
+
+var array = []
+for (var i = 15; i > 0; i -=2){
+    array.push(i);
+}
